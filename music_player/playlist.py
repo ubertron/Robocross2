6aacebd9bd46aa84a2b033f347137b8a9d40946f
ubertron <andy_j_davis@yahoo.com>
2026-03-11 18:10:10 -0700

    updated music

--- music_player/playlist.py
+++ music_player/playlist.py
@@ -2,6 +2,7 @@
 import logging
 import random
 import subprocess
+import eyed3
 
 from pathlib import Path
 from core.logging_utils import get_logger
@@ -44,9 +45,23 @@ class Playlist:
         if len(all_tracks) >= count:
             self.tracks = random.sample(all_tracks, count)
             for x in self.tracks:
-                print(x.relative_to(MUSIC_DIR))
+                LOGGER.info(x.relative_to(MUSIC_DIR))
         else:
             raise RuntimeError('Not enough tracks found')
+
+    def build_from_album(self, artist: str, album: str):
+        """Build a playlist from an album."""
+        album_dir = MUSIC_DIR / artist / album
+        if not album_dir.exists():
+            raise NotADirectoryError(f'Album {album} not found')
+        tracks = [x for x in album_dir.iterdir() if x.suffix in ('.mp3', '.m4a')]
+        if eyed3.load(tracks[0]):
+            tracks.sort(key=lambda x: eyed3.load(x).tag.track_num)
+        else:
+            tracks.sort(key=lambda x: x.as_posix())
+        for x in tracks:
+            LOGGER.info(x)
+            self.tracks.append(x)
 
     def git_add(self):
         return subprocess.check_output(['git', 'add', self.path])
@@ -58,11 +73,21 @@ class Playlist:
         self.git_add()
 
 
-def create_single_artist_playlist(artist: str, count: int = 10):
+def create_single_artist_playlist(artist: str, count: int = 10) -> Playlist:
     """Create a random playlist featuring a single artist."""
     playlist = Playlist(name=artist)
     playlist.build_from_artists(artists=[artist], count=count)
     playlist.save()
+    return playlist
+
+
+def create_playlist_from_album(artist: str, album:str):
+    """Create a random playlist featuring a single album."""
+    print("frog")
+    playlist = Playlist(name=f"{artist} - {album}")
+    playlist.build_from_album(artist=artist, album=album)
+    playlist.save()
+    return playlist
 
 
 if __name__ == "__main__":
@@ -78,4 +103,6 @@ if __name__ == "__main__":
     # beastie_boys = Playlist(name='Intergalactic')
     # beastie_boys.build_from_artists(artists=['Beastie Boys'], count=20)
     # beastie_boys.save()
-    create_single_artist_playlist(artist='Billy Idol', count=20)
+    # create_single_artist_playlist(artist='Billy Idol', count=20)
+    # create_playlist_from_album(artist="Nine Black Alps", album="Everything Is")
+    create_playlist_from_album(artist="Pendulum", album="Immersion")
